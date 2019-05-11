1f745b3779816561c95eb8de37c0ffe88ae5f88b
eravinashm <avinash532110@gmail.com>
2019-05-11 12:25:09 +0530

    Updates

---
+++ precache-manifest.d1dee31d514b2ac7fd0d51024c40ea8d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9da50d467b870ac1b7f4958685bb4f75",
+    "url": "/assignment-appiness/index.html"
+  },
+  {
+    "revision": "30c6c5c6590807dd7649",
+    "url": "/assignment-appiness/static/css/main.8d528eac.chunk.css"
+  },
+  {
+    "revision": "56143c30b9d1516152ea",
+    "url": "/assignment-appiness/static/js/2.fb8d507b.chunk.js"
+  },
+  {
+    "revision": "30c6c5c6590807dd7649",
+    "url": "/assignment-appiness/static/js/main.9c9affef.chunk.js"
+  },
+  {
+    "revision": "757cfd1033184c003fc8",
+    "url": "/assignment-appiness/static/js/runtime~main.bbf1dcb0.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/assignment-appiness/static/media/logo.5d5d9eef.svg"
+  }
+]);
